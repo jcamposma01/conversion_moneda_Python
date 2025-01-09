41b1b3606875ce580d38959a0549226cf2b98972
johncamposDigitalHouse <johnjairocampos830830@gmail.com>
2025-01-08 19:18:19 -0500

    Se realiza un programa que convierte un montos de euros a pesos mexicanos y dólares a pesos mexicanos en Python

---
+++ conversion-moneda.py
@@ -0,0 +1,26 @@
+# Paso 1: Definir el valor actual del Euro y Dolar con respecto al Peso Mexicano
+
+tipo_cambio_eur_a_mxm = 23.70  # en un caso mas realista habria que consumir informacion actualizada de BDD 0 API
+tipo_cambio_usd_a_mxm = 20.75  # en un caso mas realista habria que consumir informacion actualizada de BDD 0 API
+
+
+# Paso 2: Solicitar al usuario el; tipo de conversion(Euro a Mex o Dolar a Mex)
+
+tipo_conversion = input("Ingrese la moneda origen para la conversion(EUR/USD): ")
+
+# Paso 3: Solicitar al usuario el monto a convertir
+
+monto_convertir = float(input("Ingrese el monto a convertir: "))
+
+
+# Paso 4: Realizar la conversion utilizando el tipo de cambio correspondiente
+# Paso 5: Mostrar el resultado de la conversion  al usuario
+
+if tipo_conversion.upper() == "EUR": # en esta linea esta la funcion que convierte a mayuscula
+   resultado = monto_convertir * tipo_cambio_eur_a_mxm
+   print("El resultado de la conversion de EUR a MXM es:",resultado)
+elif tipo_conversion.upper() == "USD":
+   resultado = monto_convertir * tipo_cambio_usd_a_mxm
+   print("El resultado de la conversion de USD a MXM es:",resultado)
+else:
+   print("No esta disponible tipo de conversion en el actualmente")
