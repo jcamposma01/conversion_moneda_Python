6dea9177e4484084e37453c4fbd3c454194fa9bf
jcamposma01 <57722113+jcamposma01@users.noreply.github.com>
2025-01-08 19:21:35 -0500

    Update conversion-moneda.py

--- conversion-moneda.py
+++ conversion-moneda.py
@@ -1,3 +1,6 @@
+# Enlace de Diagrama de flujo
+#https://miro.com/welcomeonboard/bnI4dUljVkNIQTBudWl3TDlnN3o3Zi9xc3NnTGF3WTltc1JqVk8wODY4OUJMQlVsVE0yRWlpWmlFSXhNMjEyT2RLOG1EOXdJT0hYc3kyMHRMamRpY012KzY4cG5HdWp5U3B1MXlENUpuMUZWaVkrOFo2WWtRdVIrY0s1eXR0K2chZQ==?share_link_id=899620145357
+
 # Paso 1: Definir el valor actual del Euro y Dolar con respecto al Peso Mexicano
 
 tipo_cambio_eur_a_mxm = 23.70  # en un caso mas realista habria que consumir informacion actualizada de BDD 0 API
